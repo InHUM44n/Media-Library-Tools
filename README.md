e49f68499216293114b30ed822cefc3e3533332e
Robert Watter <robertwatter@gmail.com>
2025-07-12 17:00:45 +0200

    fix module creation

--- README.md
+++ README.md
@@ -2,27 +2,25 @@
 
 This is a collection of scripts I wrote to organize my media library. Maybe it's of use to someone out there.
 
-## Installing the requirements
+## Installing
 
 ```
-pip install -r requirements.txt
+git clone https://github.com/InHUM44n/Media-Library-Tools
+cd Media_Library_Tools
+pip install .
 ```
 
-Installing ffmpeg at the system-level is required for flac to mp3 conversion.
+Installing ffmpeg at the system-level is required for flac to mp3 compression.
 
 ## Functionality and Usage
 
-### Recursive conversion of FLAC to MP3
+### Audio Organizer
 
 This converts all FLAC files in the current directory and all subdirectories into MP3. ffmpeg is required here.
 ```
-python path/to/convert_all_flac_to_mp3.py
+mlt-audio path/to/folder-or-file <options>
 ```
-
-### Renaming all audio files to their Title
-
-This script fetches the track title from the metadata and renames the file to it.
+Options:
 ```
-python path/to/rename_audio_to_title.py
-    --include-track-number: adds the disc and track number to the filename
+--recursive
 ```
--- mlt_audio/cli.py
+++ mlt_audio/cli.py
@@ -3,13 +3,12 @@ import sys
 from pathlib import Path
 import argparse
 
-def apply(folderpath:Path, recursive:bool, compress_flac:bool, include_track_number:bool):
+def organize(folderpath:Path, recursive:bool, compress_flac:bool, include_track_number:bool, retain_original:bool, add_release_type:bool):
     for filepath in Path(folderpath).glob('*'):
         # if the file is a directory and recursive mode is enabled, apply actions to the files in that directory recursively.
         if os.path.isdir(filepath) and recursive:
-            apply(filepath, True, compress_flac=compress_flac, include_track_number=include_track_number)
+            organize(filepath, True, compress_flac=compress_flac, include_track_number=include_track_number, retain_original=retain_original, add_release_type=add_release_type)
         
-
 def main():
     parser = argparse.ArgumentParser(description="Organize Audio files")
 
@@ -20,7 +19,7 @@ def main():
     )
     parser.add_argument(
         "--recursive",
-        action="true_true",
+        action="store_true",
         help="apply recursively, if applied to a folder"
     )
     parser.add_argument(
@@ -33,19 +32,28 @@ def main():
         action="store_true",
         help="Compress FLAC files to MP3 files"
     )
+    parser.add_argument(
+        "--retain-original-flac",
+        action="store_true",
+        help="Do not delete the original files after conversion"
+    )
+    parser.add_argument(
+        "--add-release-type",
+        action="store_true",
+        help="Add Release type information to metadata, such as"
+    )
 
     args = parser.parse_args()
-    
-    if sys.argv == 1:
-        parser.error("No arguments provided. Use --help to see available options.")
     
     # Example usage:
     print(f"Input path: {args.path}")
     print(f"Recursive: {args.recursive}")
-    print(f"Compress FLAC: {args.compress_flac}")
     print(f"Include track: {args.include_track_number}")
+    print(f"Compress FLAC: {args.compress_flac}")
+    print(f"Retain original FLAC files: {args.retain_original_flac}")
+    print(f"Add Release Type: {args.add_release_type}")
     
     directory = Path(os.getcwd())
-    apply(directory, args.recursive, args.compress_flac, args.include_track_number)
+    organize(directory, args.recursive, args.compress_flac, args.include_track_number, args.retain_original_flac, args.add_release_type)
     
     
